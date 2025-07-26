c4c6ff61845a999029ddf16d178c0e0db7455792
lucien djob <luciendjob50@gmail.com>
2025-07-26 15:40:24 +0100

    updated api.js

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import { config } from 'dotenv';
+import mongoose from 'mongoose';
 
 // import tools db 
 import connectDB from './db/config.js';
